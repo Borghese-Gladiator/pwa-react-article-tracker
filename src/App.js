c5cf3f39fb30ad9bab549e210b8b73ead3592aef
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-09 07:14:59 -0500

    center app

--- src/App.js
+++ src/App.js
@@ -8,23 +8,21 @@ function App() {
   const [articleList, setArticleList] = useState(new Array(10).fill({}));
 
   return (
-    <div className="page-content page-container" id="page-content">
-      <div className="padding">
-        <div className="row container d-flex justify-content-center">
-          <div className="col-md-12">
-            <div className="card px-3">
-              <div className="card-body">
-                <h4 className="card-title">Awesome Todo list</h4>
-                <div className="add-items d-flex"> <input type="text" className="form-control todo-list-input" placeholder="What do you need to do today?" /> <button className="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button> </div>
-                  <div className="list-wrapper">
-                    <ArticleList articleList={articleList} />
-                  </div>
-                </div>
+    <div class="d-flex justify-content-center">
+      <div className="row container d-flex justify-content-center">
+        <div className="col-md-12">
+          <div className="card px-3">
+            <div className="card-body">
+              <h4 className="card-title">Awesome Todo list</h4>
+              <div className="add-items d-flex"> <input type="text" className="form-control todo-list-input" placeholder="What do you need to do today?" /> <button className="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button> </div>
+              <div className="list-wrapper">
+                <ArticleList articleList={articleList} />
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
