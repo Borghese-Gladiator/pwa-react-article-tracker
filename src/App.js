a482e49b35a4b156479ded87108c348a480cd382
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-09 07:29:54 -0500

    track data & enable adding new items

--- src/App.js
+++ src/App.js
@@ -5,16 +5,39 @@ import './App.css';
 import ArticleList from './components/ArticleList';
 
 function App() {
-  const [articleList, setArticleList] = useState(new Array(10).fill({}));
+  const [articleList, setArticleList] = useState(new Array(10).fill({
+    title: 'blah',
+    link: '',
+    completed: false,
+  }));
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = e => {
+    e.preventDefault();
+    console.log(e);
+    setInputValue(e.target.value);
+  }
+  const onFormSubmit = e => {
+    e.preventDefault();
+    const newElement = {
+      title: inputValue,
+      link: '',
+      completed: false
+    }
+    // push new value to end
+    setArticleList(oldArray => [...oldArray, newElement]);
+  }
 
   return (
-    <div class="d-flex justify-content-center">
+    <div className="d-flex justify-content-center">
       <div className="row container d-flex justify-content-center">
         <div className="col-md-12">
           <div className="card px-3">
             <div className="card-body">
-              <h4 className="card-title">Awesome Todo list</h4>
-              <div className="add-items d-flex"> <input type="text" className="form-control todo-list-input" placeholder="What do you need to do today?" /> <button className="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button> </div>
+              <form className="add-items d-flex" onSubmit={onFormSubmit}>
+                <input value={inputValue} onChange={handleInputChange} type="text" className="form-control todo-list-input" />
+                <button type="submit" className="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button>
+              </form>
               <div className="list-wrapper">
                 <ArticleList articleList={articleList} />
               </div>
