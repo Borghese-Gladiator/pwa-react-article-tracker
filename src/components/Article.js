6c8c6b9c5234398af7bf5add717936751e3eb2f9
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-09 10:32:36 -0500

    switch out react default favicon/logo to random flag

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,10 +3,10 @@ import { CgEnter } from 'react-icons/cg';
 export default function Article({ articleObj }) {
   const { title, link, imgLink } = articleObj;
   return (
-    <div className="col-6 col-md-4">
+    <div className="col-12 col-sm-6 col-lg-4">
       <div className="card m-2">
         <div className="d-flex align-items-center justify-content-between card-body">
-          <span>{title}{' '}<a href={link} target="_blank" rel="noopener noreferrer"><CgEnter /></a></span>
+          <a href={link} target="_blank" rel="noopener noreferrer">{title}<CgEnter /></a>
         </div>
         <img className="card-img-top" src={imgLink} alt="card graphic" />
       </div>
