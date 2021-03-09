169c1a6d8c4569df42b2b55fedb2601d5ced9f4b
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-09 10:23:01 -0500

    fix unused warnings

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,14 @@
 import { CgEnter } from 'react-icons/cg';
 
 export default function Article({ articleObj }) {
-  const { title, link, imgLink, completed } = articleObj;
+  const { title, link, imgLink } = articleObj;
   return (
     <div className="col-6 col-md-4">
       <div className="card m-2">
         <div className="d-flex align-items-center justify-content-between card-body">
           <span>{title}{' '}<a href={link} target="_blank" rel="noopener noreferrer"><CgEnter /></a></span>
         </div>
-        <img className="card-img-top" src={imgLink} alt="Card image cap" />
+        <img className="card-img-top" src={imgLink} alt="card graphic" />
       </div>
     </div>
   )
