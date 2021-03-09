ee08176583b3137bca8d493615dc3b3fcde42a40
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-09 09:24:36 -0500

    remove unused outer container

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './App.css';
 
 import Article from './components/Article';
 
+
+import PlaceholderImg from './images/bigstock-Little-Striped-Cute-Kitten-Sit.jpg';
+
 function App() {
   const [articleList, setArticleList] = useState(new Array(10).fill({
-    title: 'blah',
+    title: 'This is a really really really really cool article link',
     link: '',
+    imgLink: PlaceholderImg,
     completed: false,
   }));
   const [inputValue, setInputValue] = useState('');
@@ -22,6 +26,7 @@ function App() {
     const newElement = {
       title: inputValue,
       link: '',
+      imgLink: PlaceholderImg,
       completed: false
     }
     // push new value to end of displayed articleList
@@ -32,25 +37,21 @@ function App() {
 
   return (
     <div className="container d-flex justify-content-center">
-      <div className="col-md-12">
-        <div className="card px-3">
-          <div className="card-body">
-            <form className="add-items d-flex" onSubmit={onFormSubmit}>
-              <input value={inputValue} onChange={handleInputChange} type="text" className="form-control todo-list-input" />
-              <button type="submit" className="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button>
-            </form>
-            <div className="d-flex flex-row flex-wrap">
-              {
-                articleList.map((val, idx) => {
-                  // const { id } = articleObj;
-                  // don't just use idx (React uses keys to optimize & figure out rerenders)
-                  return (
-                    <Article key={idx} articleObj={val} />
-                  )
-                })
-              }
-            </div>
-          </div>
+      <div className="card-body">
+        <form className="add-items d-flex" onSubmit={onFormSubmit}>
+          <input value={inputValue} onChange={handleInputChange} type="text" className="form-control todo-list-input" />
+          <button type="submit" className="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button>
+        </form>
+        <div className="d-flex flex-row flex-wrap">
+          {
+            articleList.map((val, idx) => {
+              // const { id } = articleObj;
+              // don't just use idx (React uses keys to optimize & figure out rerenders)
+              return (
+                <Article key={idx} articleObj={val} />
+              )
+            })
+          }
         </div>
       </div>
     </div>
