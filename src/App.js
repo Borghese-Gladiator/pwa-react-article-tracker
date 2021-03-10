6b19111cfe3c4437929a746f180e63ae4a54df5a
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-10 15:42:41 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import PlaceholderImg from './images/bigstock-Little-Striped-Cute-Kitten-Sit.jpg
 function App() {
   const [articleList, setArticleList] = useState(new Array(3).fill({
     title: 'This is a really really really really cool article link',
-    description: '',
+    description: 'This is a really really really really cool article link. This is a really really really really cool article link',
     link: '',
     imgLink: PlaceholderImg,
     completed: false,
@@ -25,7 +25,7 @@ function App() {
     e.preventDefault();
     const newElement = {
       title: inputValue,
-      description: '',
+      description: 'This is a really really really really cool article link. This is a really really really really cool article link',
       link: '',
       imgLink: PlaceholderImg,
       completed: false
@@ -64,7 +64,7 @@ function App() {
           <input value={inputValue} onChange={handleInputChange} type="text" className="form-control todo-list-input" />
           <button type="submit" className="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button>
         </form>
-        <div className="d-flex flex-row flex-wrap">
+        <div className="d-flex flex-row flex-wrap mt-3">
           {
             articleList.map((val, idx) => {
               // const { id } = articleObj;
