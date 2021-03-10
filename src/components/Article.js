8eda414cfa5cd0f7f6b57bc2197cba8b0f7ee1da
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-03-10 15:42:37 -0500

    remove image & use description

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,14 @@
 import { CgEnter } from 'react-icons/cg';
 
 export default function Article({ articleObj }) {
-  const { title, link, imgLink } = articleObj;
+  const { title, description, link, imgLink } = articleObj;
   return (
     <div className="col-12 col-sm-6 col-lg-4">
-      <div className="card m-2">
-        <div className="d-flex align-items-center justify-content-between card-body">
-          <a href={link} target="_blank" rel="noopener noreferrer">{title}<CgEnter /></a>
+      <div className="card m-1 p-2">
+        <div class="card-body">
+          <h5 className="card-title"><a href={link} target="_blank" rel="noopener noreferrer">{title}<CgEnter /></a></h5>
+          <p class="card-text">{description}</p>
         </div>
-        <img className="card-img-top" src={imgLink} alt="card graphic" />
       </div>
     </div>
   )
